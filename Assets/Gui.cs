3891f44b70315123dbfa092dcddc15af2881b68d
kotaoue <kotaoue@gmail.com>
2013-07-17 00:50:31 +0900

    [kotaoue]+ all files

--- Assets/Gui.cs
+++ Assets/Gui.cs
@@ -8,9 +8,11 @@ using System;
  * @author kotaoue@gmail.com
  */
 public class Gui : MonoBehaviour {
+	public GUIStyle guiStyle;
+	
 	private void OnGUI() {
 		// Make normal clock.
-		GUILayout.Box(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"), GUILayout.Width(Screen.width));
+		// GUILayout.Box(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"), GUILayout.Width(Screen.width));
 		/*
 		GUILayout.BeginArea(new Rect(0, 0, Screen.width, 40));
 			GUILayout.BeginHorizontal();
@@ -19,5 +21,19 @@ public class Gui : MonoBehaviour {
 			GUILayout.EndHorizontal();
 	    GUILayout.EndArea();
 	    */
+		
+		GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height * 0.25f));
+			GUILayout.BeginHorizontal();
+				GUILayout.Label(Mathf.Floor(Time.time).ToString(), guiStyle, GUILayout.Width(Screen.width * 0.95f));
+				GUILayout.FlexibleSpace();
+			GUILayout.EndHorizontal();
+			
+			GUILayout.FlexibleSpace();
+		
+			GUILayout.BeginHorizontal();
+				GUILayout.Label(Mathf.Floor(Time.time / 30).ToString(), guiStyle, GUILayout.Width(Screen.width * 0.95f));
+				GUILayout.FlexibleSpace();
+			GUILayout.EndHorizontal();
+	    GUILayout.EndArea();
 	}
 }
--- Assets/MainScene.cs
+++ Assets/MainScene.cs
@@ -8,24 +8,35 @@ using System;
  * @author kotaoue@gmail.com
  */
 public class MainScene : MonoBehaviour {
+	public GUIStyle guiStyle;
+	private float startTime;
 
 	// Use this for initialization
 	void Start () {
-	
+		startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetMouseButtonDown(0)) {
+			startTime = Time.time;
+		}	
 	}
 	
-	void OnGUI() {
-		GUILayout.BeginArea(new Rect(0, 50, Screen.width, 40));
+	private void OnGUI() {
+		GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height * 0.25f));
 			GUILayout.BeginHorizontal();
-				GUILayout.Box(Time.time.ToString(), GUILayout.Width(100));
+				GUILayout.Label(Mathf.Floor((Time.time - startTime) % 30.0f).ToString(), guiStyle, GUILayout.Width(Screen.width * 0.95f));
 				GUILayout.FlexibleSpace();
-				GUILayout.Box(Mathf.Floor(Time.time / 30).ToString(), GUILayout.Width(100));
+			GUILayout.EndHorizontal();
+			
+			GUILayout.FlexibleSpace();
+		
+			GUILayout.BeginHorizontal();
+				GUILayout.Label(Mathf.Floor((Time.time - startTime) / 30.0f).ToString(), guiStyle, GUILayout.Width(Screen.width * 0.95f));
+				GUILayout.FlexibleSpace();
 			GUILayout.EndHorizontal();
 	    GUILayout.EndArea();
 	}
+
 }
